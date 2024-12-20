6be1756e299b7a81b1ed04b0d20eb544ade119e9
Felice Pantaleo <felice.pantaleo@cern.ch>
2024-12-20 14:10:14 +0100

    Working version with flow graph

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 clean:
-	rm -rf test 
+	rm -rf test
 
 
-all: 
-  g++ test.cpp -std=c++20 -o test -O2 -I /cvmfs/cms.cern.ch/el8_amd64_gcc12/external/tbb/v2021.9.0-573155027234b8f945d29403a2749d52/include -L /cvmfs/cms.cern.ch/el8_amd64_gcc12/external/tbb/v2021.9.0-573155027234b8f945d29403a2749d52/lib 
+all:
+	g++ test_priority.cpp -std=c++20 -o test -O2 -I /cvmfs/cms.cern.ch/el8_amd64_gcc12/external/tbb/v2021.9.0-573155027234b8f945d29403a2749d52/include -L /cvmfs/cms.cern.ch/el8_amd64_gcc12/external/tbb/v2021.9.0-573155027234b8f945d29403a2749d52/lib/ -ltbb -pthread
 
 
 
--- test_priority.cpp
+++ test_priority.cpp
@@ -1,47 +1,164 @@
+#include <tbb/flow_graph.h>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+#include <tbb/tbb.h>
+
 #include <chrono>
 #include <iostream>
+#include <random>
 #include <thread>
+#include <vector>
+#include <map>
 
-#include <tbb/parallel_for.h>
-#include <tbb/task_arena.h>
-#include <tbb/this_task_arena.h>
+static void sleep_ms(int ms) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
 
 int main() {
-  // High-priority arena (for letters)
-  tbb::task_arena highPriArena(
-    tbb::task_arena::automatic,
-    tbb::task_arena::automatic,
-    tbb::priority_high
-  );
+  using namespace tbb::flow;
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 1);
+  graph g;
+  std::map<int, int> priority_map;
+  priority_map[1] = 1;
+  priority_map[2] = 2;
+  priority_map[3] = 2;
+  priority_map[4] = 2;
+  priority_map[5] = 1;
+  priority_map[6] = 0;
+  priority_map[7] = 2;
 
-  // Low-priority arena (for numbers)
-  tbb::task_arena lowPriArena(
-    tbb::task_arena::automatic,
-    tbb::task_arena::automatic,
-    tbb::priority_low
-  );
-
-  highPriArena.execute([&]() {
+  std::map<int, std::string> priority_str;
+  priority_str[0] = "low";
+  priority_str[1] = "medium";
+  priority_str[2] = "high";
 
 
-    // Now nest a low-priority arena execution
-    lowPriArena.execute([&]() {
-      tbb::this_task_arena::isolate([&]() {
-        // Print numbers 0-9 in parallel
-        tbb::parallel_for(size_t(0), size_t(10), [&](size_t i) {
-          std::cout << "Number: " << i << "\n";
-        });
-      });
-    });
+  // Helper lambdas for printing start/end with priority
+  auto print_begin = [](int task_number, const char* priority) {
+    std::cout << "Begin Task " << task_number << " (priority: " << priority << ")\n";
+  };
+  auto print_end = [](int task_number, const char* priority) {
+    std::cout << "End Task " << task_number << " (priority: " << priority << ")\n";
+  };
 
-    // Run a set of tasks that print letters A-J, each with a delay
-    tbb::parallel_for(size_t(0), size_t(10), [&](size_t i) {
-      char letter = static_cast<char>('A' + i);
-      // Simulate work
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
-      std::cout << "Letter: " << letter << "\n";
-    });
-  });
+  // Task 1: print A,B,C and sleep 2s, priority medium
+  continue_node<continue_msg> t1(g, [&](const continue_msg&) {
+    int task_number = 1;
+    print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+    std::cout << "A\nB\nC\n";
+    sleep_ms(2000);
+    print_end(task_number, priority_str[priority_map[task_number]].c_str());
+    return continue_msg();
+  }, node_priority_t(priority_map[1]));
+
+  // Task 2: print D,E,F,G and sleep 3s, priority high
+  continue_node<continue_msg> t2(g, [&](const continue_msg&) {
+    int task_number = 2;
+    print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+    std::cout << "D\nE\nF\nG\n";
+    sleep_ms(3000);
+    print_end(task_number, priority_str[priority_map[task_number]].c_str());
+    return continue_msg();
+  }, node_priority_t(priority_map[2]));
+
+  // Task 3: print H to L and sleep 1s, priority high
+  continue_node<continue_msg> t3(g, [&](const continue_msg&) {
+    int task_number = 3;
+    print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+    for (char c = 'H'; c <= 'L'; ++c) {
+      std::cout << c << "\n";
+    }
+    sleep_ms(1000);
+    print_end(task_number, priority_str[priority_map[task_number]].c_str());
+    return continue_msg();
+  }, node_priority_t(priority_map[3]));
+
+  // Task 4: print M to Z and sleep 2s, priority high
+  continue_node<continue_msg> t4(g, [&](const continue_msg&) {
+    int task_number = 4;
+    print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+    for (char c = 'M'; c <= 'Z'; ++c) {
+      std::cout << c << "\n";
+    }
+    sleep_ms(2000);
+    print_end(task_number, priority_str[priority_map[task_number]].c_str());
+    return continue_msg();
+  }, node_priority_t(priority_map[4]));
+
+  // Task 5: produce a vector<int> of 200 random ints, priority low
+  function_node<continue_msg, std::vector<int>> t5(g, serial, [&](const continue_msg&) {
+    int task_number = 5;
+    print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+    std::vector<int> v(200);
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<int> dist(0, 1000);
+    for (auto &val : v) {
+      val = dist(gen);
+    }
+    sleep_ms(100); // Just a small sleep to simulate work
+    print_end(task_number, priority_str[priority_map[task_number]].c_str());
+    return v;
+  }, node_priority_t(priority_map[5]));
+
+// Task 6: should print the content of the vector produced by task 5
+// We'll use a function_node to take std::vector<int> as input and produce continue_msg as output.
+function_node<std::vector<int>, continue_msg> t6(
+    g, 
+    serial, 
+    [&](const std::vector<int>& vec) {
+      int task_number = 6;
+      print_begin(task_number, priority_str[priority_map[task_number]].c_str()); 
+      tbb::parallel_for(
+        tbb::blocked_range<size_t>(0, vec.size()),
+        [&](const tbb::blocked_range<size_t>& r) {
+          for (size_t i = r.begin(); i < r.end(); ++i) {
+            std::cout << "Task 6 printing index " << i << ": " << vec[i] << "\n";
+          }
+        }
+      );
+      print_end(task_number, priority_str[priority_map[task_number]].c_str());
+      return continue_msg();
+    },
+    node_priority_t(priority_map[6]) // If you want to set a priority
+);
+
+
+  // Task 7: print "ciao" and sleep for 5 seconds. priority high
+  // Task 7 depends on task 5 and 4.
+  // We have a join node to combine outputs from 5 (vector<int>) and 4 (continue_msg).
+  join_node<std::tuple<std::vector<int>, continue_msg>, queueing> j(g);
+
+  function_node<std::tuple<std::vector<int>, continue_msg>, continue_msg> t7(
+      g, serial, [&](const std::tuple<std::vector<int>, continue_msg>&) {
+        int task_number = 7;
+        print_begin(task_number, priority_str[priority_map[task_number]].c_str());
+        std::cout << "ciao\n";
+        sleep_ms(5000);
+        print_end(task_number, priority_str[priority_map[task_number]].c_str());
+        return continue_msg();
+      }, node_priority_t(priority_map[7]));
+
+  // Now we set up dependencies:
+  // The chain: 1 -> 2 -> 3 -> 4
+  make_edge(t1, t2);
+  make_edge(t2, t3);
+  make_edge(t3, t4);
+
+  // Task 6 depends on 5
+  make_edge(t5, t6);
+
+  // Task 7 depends on 5 and 4: connect them to join node
+  make_edge(t5, input_port<0>(j));
+  make_edge(t4, input_port<1>(j));
+
+  // connect join node to t7
+  make_edge(j, t7);
+
+  // Start the flow by activating task 1 and task 5
+  t1.try_put(continue_msg());
+  t5.try_put(continue_msg());
+
+  g.wait_for_all();
 
   return 0;
 }
